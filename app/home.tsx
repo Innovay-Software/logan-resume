0f6bd24ddb156bec5def03c943208d2977fdafd5
ealton <longjdai@gmail.com>
2024-11-21 12:23:59 -0500

    Adding 't' searchParam to home url

--- app/home.tsx
+++ app/home.tsx
@@ -1,15 +1,16 @@
 "use client";
-import "./globals.css";
 
+import { useSearchParams } from 'next/navigation';
+import { useState } from "react";
 import ResumeSideBar from "./components/ResumeSideBar";
 import ResumeTopBar from "./components/ResumeTopBar";
 import ResumePanel from "./components/ResumePanel";
 import PortfolioPanel from "./components/PortfolioPanel";
-import { useState } from "react";
 import { UserDataInterface } from "./interfaces/userDataInterface";
 
 export default function Home({ userData }: { userData: UserDataInterface }) {
-  const [currentPageIndex, setCurrentPageIndex] = useState(0);
+  const searchParams = useSearchParams();
+  const [currentPageIndex, setCurrentPageIndex] = useState(searchParams.get("t") == "1"? 1 : 0);
   return (
     <main className="flex items-center justify-center bg-slate-100 p-8">
       <div className="max-w-screen-lg grid grid-cols-6 gap-8">
