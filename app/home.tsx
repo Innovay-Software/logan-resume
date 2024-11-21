5f7d16c06edc4c2353baea3a414d80eb9dad7e39
ealton <longjdai@gmail.com>
2024-11-21 12:32:47 -0500

    Fixed 'useSearchParams() should be wrapped in a suspense boundary' error

--- app/home.tsx
+++ app/home.tsx
@@ -1,32 +1,44 @@
 "use client";
 
-import { useSearchParams } from 'next/navigation';
-import { useState } from "react";
+import { useSearchParams } from "next/navigation";
+import { useState, Suspense } from "react";
 import ResumeSideBar from "./components/ResumeSideBar";
 import ResumeTopBar from "./components/ResumeTopBar";
 import ResumePanel from "./components/ResumePanel";
 import PortfolioPanel from "./components/PortfolioPanel";
 import { UserDataInterface } from "./interfaces/userDataInterface";
 
-export default function Home({ userData }: { userData: UserDataInterface }) {
+function Home({ userData }: { userData: UserDataInterface }) {
   const searchParams = useSearchParams();
-  const [currentPageIndex, setCurrentPageIndex] = useState(searchParams.get("t") == "1"? 1 : 0);
-  return (
-    <main className="flex items-center justify-center bg-slate-100 p-8">
-      <div className="max-w-screen-lg grid grid-cols-6 gap-8">
-        <div className="col-span-6 sm:col-span-3 md:col-span-2">
-          <ResumeSideBar userData={userData} />
-        </div>
-        <div className="col-span-6 sm:col-span-3 md:col-span-4">
-          <ResumeTopBar
-            userData={userData}
-            currentIndex={currentPageIndex}
-            onButtonClick={setCurrentPageIndex}
-          />
-          {currentPageIndex == 0 && <ResumePanel userData={userData} />}
-          {currentPageIndex == 1 && <PortfolioPanel userData={userData} />}
-        </div>
-      </div>
-    </main>
+  const [currentPageIndex, setCurrentPageIndex] = useState(
+    searchParams.get("t") == "1" ? 1 : 0
   );
+  return (
+    <Suspense>
+      <main className="flex items-center justify-center bg-slate-100 p-8">
+        <div className="max-w-screen-lg grid grid-cols-6 gap-8">
+          <div className="col-span-6 sm:col-span-3 md:col-span-2">
+            <ResumeSideBar userData={userData} />
+          </div>
+          <div className="col-span-6 sm:col-span-3 md:col-span-4">
+            <ResumeTopBar
+              userData={userData}
+              currentIndex={currentPageIndex}
+              onButtonClick={setCurrentPageIndex}
+            />
+            {currentPageIndex == 0 && <ResumePanel userData={userData} />}
+            {currentPageIndex == 1 && <PortfolioPanel userData={userData} />}
+          </div>
+        </div>
+      </main>
+    </Suspense>
+  );
+}
+
+export default function HomeWrapper({ userData }: { userData: UserDataInterface }) {
+  return (
+    <Suspense>
+      <Home userData={userData} />
+    </Suspense>
+  )
 }
